90532235b073cf219f4956b32e770a3293bc1d15
marcarballo97 <79873493+marcarballo97@users.noreply.github.com>
2023-02-24 14:47:23 -0600

    Add files via upload

---
+++ tailwind tarea practica/js/script.js
@@ -0,0 +1,19 @@
+
+tailwind.config={
+    theme:{
+        extend:{
+            colors:{
+                nude: "#f1ede6",
+                sky: "#075985",
+                frosted: "#768a96",
+                cream: "#d6bcae",
+                jade:"#7e7f7a",
+                orchid:"#c1abad",
+                supple: "#c39e9e",
+                bone:"#f6f5f0",
+            }
+
+        },
+
+    },
+}
